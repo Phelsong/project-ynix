44a76a1cf63450772a592933acfa8e330bb3bdcd
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-07-11 18:33:22 -0500

    setup part2

--- ynix_b/DB/Index.py
+++ ynix_b/DB/Index.py
@@ -1,27 +1,30 @@
 import psycopg2
 from dotenv import dotenv_values
-#----------------------------------------------------------------
-config = dotenv_values(".env")
-print(config['USER_NAME'])
+# ----------------------------------------------------------------
+config = dotenv_values("../../.env")
 myUser = config['USER_NAME']
 myPassword = config['PASS_WORD']
-print(config)
-#----------------------------------------------------------------
-connect = psycopg2.connect(database = "py-lab", user = myUser, password = myPassword, host="localhost", port="5432")
+# ----------------------------------------------------------------
+connect = psycopg2.connect(database="ynix_db", user=myUser,
+                           password=myPassword, host="localhost", port="5432")
 cursor = connect.cursor()
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
+
 
 def drop_tables():
-    cursor.execute('''CREATE TABLE IF NOT EXISTS users
-               (user_id serial PRIMARY KEY''')
+    cursor.execute('''DROP TABLE IF EXISTS users''')
+
 
 def create_tables():
-    cursor.execute('''CREATE TABLE IF NOT EXISTS users
-               (user_id serial PRIMARY KEY''')
-    
-##----------------------------------------------------------------    
-drop_tables() 
+    cursor.execute('''CREATE TABLE users 
+                    (user_id serial PRIMARY KEY
+                    , username varchar(100),
+                    password varchar(100))''')
+
+
+# ----------------------------------------------------------------
+drop_tables()
 create_tables()
 connect.commit()
 connect.close()
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
