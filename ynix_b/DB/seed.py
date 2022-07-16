3fef4e3ebafdaee395ae31b991d69d5e8a6ff815
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-07-16 14:41:08 -0500

    seed

--- ynix_b/DB/seed.py
+++ ynix_b/DB/seed.py
@@ -2,7 +2,7 @@ from Index import *
 # ----------------------------------------------------------------
 
 def drop_tables():
-    cursor.execute('''DROP TABLE IF EXISTS users,
+    cursor.execute('''DROP TABLE IF EXISTS users;
                    DROP TABLE IF EXISTS classes''')
 
 def create_tables():
@@ -11,35 +11,36 @@ def create_tables():
                     , username varchar(100),
                     password varchar(200))''')
     cursor.execute('''CREATE TABLE classes
-                    (user_id serial PRIMARY KEY
-                    , name varchar(100))''')
+                    (id serial PRIMARY KEY
+                    , name varchar(150))''')
 
 def class_seed():
-    cursor.execute('''INSERT INTO classes (id, name),
-        (1, "Warrior"),
-        (2, "Ranger"),
-        (3, "Sorceress"),
-        (4, "Berserker"),
-        (5, "Tamer"),
-        (6, "Musa"),
-        (7, "Maewha"),
-        (8, "Valkyrie"),
-        (9, "Kunoichi"),
-        (10, "Ninja"),
-        (11, "Wizard"),
-        (12, "Witch"),
-        (13, "Mystic"),
-        (14, "Striker"),
-        (15, "Dark Knight"),
-        (16, "Lahn"),
-        (17, "Archer"),
-        (18, "Shai"),
-        (19, "Guardian"),
-        (20, "Hashashin"),
-        (21, "Nova"),
-        (22, "Sage"),
-        (23, "Corsair"),
-        (24, "Drakania"),''')
+    cursor.execute('''INSERT INTO classes (id, name)
+                   VALUES
+        (1, 'Warrior'),
+        (2, 'Ranger'),
+        (3, 'Sorceress'),
+        (4, 'Berserker'),
+        (5, 'Tamer'),
+        (6, 'Musa'),
+        (7, 'Maewha'),
+        (8, 'Valkyrie'),
+        (9, 'Kunoichi'),
+        (10, 'Ninja'),
+        (11, 'Wizard'),
+        (12, 'Witch'),
+        (13, 'Mystic'),
+        (14, 'Striker'),
+        (15, 'Dark Knight'),
+        (16, 'Lahn'),
+        (17, 'Archer'),
+        (18, 'Shai'),
+        (19, 'Guardian'),
+        (20, 'Hashashin'),
+        (21, 'Nova'),
+        (22, 'Sage'),
+        (23, 'Corsair'),
+        (24, 'Drakania')''')
 
 
 # ----------------------------------------------------------------
