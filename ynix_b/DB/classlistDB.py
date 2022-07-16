8f71ba249cccbb43e89276d5b0f5419cc7348d34
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-07-16 14:33:56 -0500

    seed

--- ynix_b/DB/classlistDB.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-from Index import *
-
-
-def class_seed():
-    cursor.execute('''INSERT INTO classes,
-                   ''')
--- ynix_b/DB/seed.py
+++ ynix_b/DB/seed.py
@@ -5,7 +5,6 @@ def drop_tables():
     cursor.execute('''DROP TABLE IF EXISTS users,
                    DROP TABLE IF EXISTS classes''')
 
-
 def create_tables():
     cursor.execute('''CREATE TABLE users 
                     (user_id serial PRIMARY KEY
@@ -15,10 +14,38 @@ def create_tables():
                     (user_id serial PRIMARY KEY
                     , name varchar(100))''')
 
+def class_seed():
+    cursor.execute('''INSERT INTO classes (id, name),
+        (1, "Warrior"),
+        (2, "Ranger"),
+        (3, "Sorceress"),
+        (4, "Berserker"),
+        (5, "Tamer"),
+        (6, "Musa"),
+        (7, "Maewha"),
+        (8, "Valkyrie"),
+        (9, "Kunoichi"),
+        (10, "Ninja"),
+        (11, "Wizard"),
+        (12, "Witch"),
+        (13, "Mystic"),
+        (14, "Striker"),
+        (15, "Dark Knight"),
+        (16, "Lahn"),
+        (17, "Archer"),
+        (18, "Shai"),
+        (19, "Guardian"),
+        (20, "Hashashin"),
+        (21, "Nova"),
+        (22, "Sage"),
+        (23, "Corsair"),
+        (24, "Drakania"),''')
+
 
 # ----------------------------------------------------------------
 drop_tables()
 create_tables()
+class_seed()
 connect.commit()
 connect.close()
 # ----------------------------------------------------------------
