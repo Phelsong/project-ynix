18276e3f3fc4e52b4958e32951eca3249c98bb1f
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-07-16 13:48:41 -0500

    stuff

--- src/Components/Main_Body/SimSettings.jsx
+++ src/Components/Main_Body/SimSettings.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import {ClassDropdown} from "../index"
 //----------------------------------------------------------------
 
 //----------------------------------------------------------------
@@ -63,34 +64,10 @@ const SimSettings = () => {
         <input type="text" />
       </form>
       <form className="sim-settings">
-        {/* This probably needs to be broken out into a seperate component, revisit latter **** */}
         Attacker - Class
-        <select type="dropdown">
-          <option value="1">Warrior</option>
-          <option value="2">Ranger</option>
-          <option value="3">Sorceress</option>
-          <option value="4">Berserker</option>
-          <option value="5">Tamer</option>
-          <option value="6">Musa</option>
-          <option value="7">Maewha</option>
-          <option value="8">Valkyrie</option>
-          <option value="9">Kunoichi</option>
-          <option value="10">Ninja</option>
-          <option value="11">Wizard</option>
-          <option value="12">Witch</option>
-          <option value="13">Mystic</option>
-          <option value="14">Striker</option>
-          <option value="15">Lahn</option>
-          <option value="16">Archer</option>
-          <option value="17">Dark Knight</option>
-          <option value="18">Shai</option>
-          <option value="19">Guardian</option>
-          <option value="20">Hashashin </option>
-          <option value="21">Nova</option>
-          <option value="22">Sage</option>
-          <option value="23">Corsair</option>
-          <option value="24">Drakania</option>
-        </select>
+       <ClassDropdown />
+       Defender - PvP
+       <ClassDropdown />
       </form>
       <button className="btn btn-primary" onClick={e => navTo('/Results')}> Run </button>
     </div>
--- src/Components/index.js
+++ src/Components/index.js
@@ -3,7 +3,7 @@ export { default as SimSettings } from './Main_Body/SimSettings'
 export { default as Results } from './Main_Body/Results'
 export { default as UserSettings } from './Main_Body/UserSettings'
 export { default as Profile} from './Main_Body/Profile'
-
+export { default as ClassDropdown } from './Main_Body/ClassDropdown'
 
 
 
