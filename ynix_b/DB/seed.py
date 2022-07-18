01530a2306f4e9a07192b8167a4599fb657ac181
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-07-18 17:31:06 -0500

    seed more

--- ynix_b/DB/seed.py
+++ ynix_b/DB/seed.py
@@ -1,27 +1,32 @@
 from Index import *
 # ----------------------------------------------------------------
 
+
 def drop_tables():
     cursor.execute('''DROP TABLE IF EXISTS users;
                    DROP TABLE IF EXISTS classes;
                    DROP TABLE IF EXISTS class_skills;''')
 
+
 def create_tables():
     cursor.execute('''CREATE TABLE users 
                     (user_id serial PRIMARY KEY
-                    , username varchar(100),
+                    , username varchar(100) NOT NULL,
                     password varchar(200))''')
     cursor.execute('''CREATE TABLE classes
-                    (id serial PRIMARY KEY
-                    , name varchar(150))''')
-    cursor.execute('''CREATE TABLE class_skills,
-                   skill_name varchar(200),
-                   skill_acc int,
-                   skill_details JSONB;
+                    (class_id SERIAL PRIMARY KEY ,
+                    class_name VARCHAR(150) NOT NULL)''')
+    cursor.execute('''CREATE TABLE class_skills
+                   (skill_id SERIAL PRIMARY KEY,
+                   "class_id" INT REFERENCES classes(class_id) NOT NULL,
+                   skill_name VARCHAR(200) NOT NULL,
+                   skill_acc INT NOT NULL,
+                   skill_details JSONB);
                    ''')
 
+
 def class_seed():
-    cursor.execute('''INSERT INTO classes (id, name)
+    cursor.execute('''INSERT INTO classes (class_id, class_name)
                    VALUES
         (1, 'Warrior'),
         (2, 'Ranger'),
