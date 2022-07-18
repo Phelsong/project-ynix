4310947ebf55d97310cb3b4e6fd352ff6eefe5ce
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-07-18 17:47:17 -0500

    seed data

---
+++ ynix_b/DB/Shai_data.py
@@ -0,0 +1,34 @@
+
+Skills = {
+    'One_Two_Three': {
+        'skill_id': 3866,
+        'skill_accuracy': .11,
+        'skill_details': {
+            'hit1': 889,
+            'hit1_count':29,
+            'hit1_crit_pve': 0,
+            'hit1_crit_pvp': 0,
+            'hit1_pvp_mod': .273,
+            'hit2': 889,
+            'hit2_count':27,
+            'hit2_crit_pve': 0,
+            'hit2_crit_pvp': 0,
+            'hit2_pvp_mod': .273,
+            'hit3': 792,
+            'hit3_count': 37,
+            'hit3_crit_pve': .5,
+            'hit3_crit_pvp': 0,
+            'hit3_pvp_mod': .237
+        }
+    },
+    'Eat_This_VI': {
+        'skill_id': 3884,
+        'skill_acc': .05,
+        'skill_details': {
+            'hit1': 1452,
+            'hit1_count': 4,
+            'hit1_crit_pve': 1,
+            'hit1_crit_pvp': 0,
+            'hit1_pvp_mod': .2},
+    }
+}
