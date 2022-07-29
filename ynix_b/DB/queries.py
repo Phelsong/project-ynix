5bd420225450ad229c37e77dc7d4d488cdae89e0
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-07-28 21:30:37 -0500

    syntax fix

--- ynix_b/DB/queries.py
+++ ynix_b/DB/queries.py
@@ -1,13 +1,15 @@
 from __init__ import cur, conn
 #----------------------------------------------------------------
 
-    
 def get_class_list():
     cur.execute('''SELECT * FROM classes''')
     class_list_fetch = cur.fetchall()
-    print(class_list_fetch)
     return class_list_fetch
+#----------------------------------------------------------------
 
-get_class_list()
 
+
+
+
+#----------------------------------------------------------------
 conn.commit()
