8abe1066942ed71f55bc4dabdd068be1838e8ee0
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-08-18 14:45:35 -0500

    deployment build v1

--- ynix_b/src/DB/connection.py
+++ ynix_b/src/DB/connection.py
@@ -4,11 +4,13 @@ from psycopg.rows import dict_row
 
 # ----------------------------------------------------------------
 config = dotenv_values("../../../.env")
-my_user = config["USER_NAME"]
-my_pass = config["PASS_WORD"]
-lab_db = config["LAB_DB"]
-lab_db_server = config["LAB_DB_SERVER"]
-lab_db_port = config["LAB_DB_PORT"]
+
+
+my_user = config["USER_NAME"] if not None else "postgres"
+my_pass = config["PASS_WORD"] if not None else None
+lab_db = config["LAB_DB"] if True else "ynix-app"
+lab_db_server = config["LAB_DB_SERVER"] if not None else config["DATABASE_URL"]
+lab_db_port = config["LAB_DB_PORT"] if not None else '5432'
 
 # ----------------------------------------------------------------
 conn = psycopg.connect(
