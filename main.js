60919434ffd87c3d4a0b5d504c9fc08bd39cc208
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-09-01 19:54:50 -0500

    undo

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 1200,
         height: 900,
-        webPreferences: {
-          preload: path.join(__dirname, '../preload.js')
-        }
+        // webPreferences: {
+        //   preload: path.join(__dirname, './preload.js')
+        // }
       })
       
-      win.loadURL('http://localhost:3000/')
-      // win.loadURL(`file:${path.join(__dirname, '../build/index.html')}`)
+      // win.loadURL('http://localhost:3000/')
+      win.loadURL(`file:${path.join(__dirname, './build/index.html')}`)
 }
 
 //----------------------------------------------------------------
